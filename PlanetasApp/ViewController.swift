7dab53bfe2abe9114990f3a5609be08f45cddc48
Teo <teo.calle.lara@gmail.com>
2023-01-03 08:43:42 -0500

    Initial Commit

---
+++ PlanetasApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanetasApp
+//
+//  Created by ADMIN on 3/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
